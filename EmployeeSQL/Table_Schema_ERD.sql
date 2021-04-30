dcfc2f539816f1d5e1527583c74c300f28a1c64f
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-04-30 13:21:33 -0600

    started query code, but need start and end date in some tables for queries

---
+++ EmployeeSQL/Table_Schema_ERD.sql
@@ -0,0 +1,79 @@
+-- Drop Tables if it exists in databases
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_employees;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Create a table schema for each of the six CSV files. 
+-- Specify data types, primary keys, foreign keys, and other constraints.
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(20)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    sex VARCHAR(2)   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(4)   NOT NULL,
+    dept_name VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    id serial   NOT NULL,
+    emp_no int NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (id)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(7)   NOT NULL,
+    emp_no INT   NOT NULL
+);
+
+CREATE TABLE salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(10)   NOT NULL,
+    title VARCHAR(255)   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (title_id)
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+-- See all tables
+select * from departments;
+select * from employees;
+select * from dept_emp;
+select * from dept_manager;
+select * from salaries;
+select * from titles;
