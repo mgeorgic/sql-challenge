da8703029b50b82e7a7f4c27c3f3578aa242e098
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-04-30 20:15:58 -0600

    Query code complete

--- EmployeeSQL/Query.sql
+++ EmployeeSQL/Query.sql
@@ -38,3 +38,61 @@ LEFT JOIN employees as e
 on dm.emp_no = e.emp_no
 ORDER BY emp_no;
 
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+
+SELECT
+e.emp_no,
+e.last_name,
+e.first_name,
+de.dept_no,
+d.dept_name
+FROM employees as e
+INNER JOIN dept_emp as de
+on e.emp_no = de.emp_no
+INNER JOIN departments as d
+on de.dept_no = d.dept_no
+ORDER BY emp_no;
+
+-- 5. List first name, last name, and sex for employees whose 
+-- first name is "Hercules" and last names begin with "B."
+SELECT 
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their 
+-- employee number, last name, first name, and department name.
+SELECT
+e.emp_no,
+e.last_name,
+e.first_name,
+de.dept_no
+FROM employees as e
+LEFT JOIN dept_emp as de
+on e.emp_no = de.emp_no
+-- use where statement for sales department  only
+INNER JOIN departments as d
+on d.dept_no=de.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development')
+
+-- 8. In descending order, list the frequency count of employee last 
+-- names, i.e., how many employees share each last name.
+SELECT last_name,
+COUNT(*) AS freq_last_name
+FROM employees
+GROUP BY last_name
+ORDER BY freq_last_name DESC;
